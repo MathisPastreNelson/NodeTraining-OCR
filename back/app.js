a8286c208e5281193ef2fc7a3d548a147c7b5923
MathisPastreNelson <shiray@hotmail.fr>
2022-09-26 02:05:38 +0200

    Ajout middleware pour création d'un article

--- back/app.js
+++ back/app.js
@@ -5,12 +5,22 @@ const app = express();
 
 
 //Middleware
+app.use(express.json());
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
+
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+        message: "Objet crée !"
+    })
+}
+)
 
 app.use('/api/stuff', (req, res, next) => {
     const stuff = [
